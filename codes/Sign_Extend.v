99bd75467ad07b9c273ee2824accae55f42f58bd
waffle <b07902011@ntu.edu.tw>
2020-12-11 22:31:35 +0800

    fix sign

--- codes/Sign_Extend.v
+++ codes/Sign_Extend.v
@@ -4,8 +4,20 @@ module Sign_Extend(
 );
 
 input   [31:0]  data_i;
-output  [31:0]  data_o;
+output [31:0]  data_o;
 
-assign data_o = {{20{data_i[11]}},data_i[11:0]};
+reg [31:0] data_o;
+
+always@(data_i)
+    begin
+        case (data_i[6:0])
+            7'b0010011: data_o = {{20{data_i[31]}}, data_i[31:20]};
+            7'b0100011: data_o = {{20{data_i[31]}}, data_i[31:25], data_i[4:0]};
+            7'b1100011: data_o = {{20{data_i[31]}}, data_i[31], data_i[0], data_i[30:25], data_i[4:1]};
+            7'b0000011: data_o = {{20{data_i[31]}}, data_i[31:20]};
+            default: data_o = 0;
+        endcase        
+    end
+
 
 endmodule 
