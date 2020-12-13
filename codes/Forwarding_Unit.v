41cd8b3d93a7529c8d557d7b9a07040085dfcb50
waffle <b07902011@ntu.edu.tw>
2020-12-13 11:33:06 +0800

    MUX_4 and foward unit inside

--- codes/Forwarding_Unit.v
+++ codes/Forwarding_Unit.v
@@ -9,4 +9,23 @@ module{
     ForwardB_o,
 };
 
+input [4:0] Rs1_i, Rs2_i, WB_Rd_i, MEM_Rd_i;
+input WB_RegWrite_i, MEM_RegWrite_i;
+output reg [1:0] ForwardA_o, ForwardB_o;
+
+always@(Rs1_i, Rs2_i, WB_Rd_i, MEM_Rd_i, WB_RegWrite_i, MEM_RegWrite_i)
+    begin
+        ForwardA_o = 2'b00;
+        ForwardB_o = 2'b00;
+        if (MEM_RegWrite_i && MEM_Rd_i != 0 && MEM_Rd_i == Rs1_i) ForwardA_o = 2'b10;
+        else if (WB_RegWrite_i && WB_Rd_i != 0 && WB_Rd_i == Rs1_i) ForwardA_o = 2'b01;
+        else ForwardA_o = 2'b00;
+        
+        if (MEM_RegWrite_i && MEM_Rd_i != 0 && MEM_Rd_i == Rs2_i) ForwardB_o = 2'b10;
+        else if (WB_RegWrite_i && WB_Rd_i != 0 && WB_Rd_i == Rs2_i) ForwardB_o = 2'b01;
+        else ForwardB_o = 2'b00;
+
+        
+    end
+
 endmodule
--- codes/MUX_4.v
+++ codes/MUX_4.v
@@ -6,4 +6,20 @@ module{
     select_i,
     data_o
 };
+
+input [31:0] input_00_i, input_01_i, input_10_i, input_11_i;
+input [1:0] select_i;
+output reg data_o;
+
+always@(select_i, input_00_i input_01_i, input_10_i, input_11_i)
+    begin
+        case (select_i)
+            2'b00: data_o <= input_00_i; 
+            2'b01: data_o <= input_01_i;
+            2'b10: data_o <= input_10_i;
+            2'b11: data_o <= input_11_i;
+            default: data_o = 0;
+        endcase
+    end
+
 endmodule;
